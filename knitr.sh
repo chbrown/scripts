4165621581567b9842827e47049f22a99c628574
Christopher Brown <io@henrian.com>
2013-03-12 02:28:06 -0500

    Add knitr.sh script to go with ptx.sh and tx.sh scripts

---
+++ knitr.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+RTEXFILES=($1)
+RTEXFILE=${RTEXFILES[0]/\.Rtex/}
+echo Knitting $RTEXFILE
+Rscript -e "library(knitr); knit(\"$RTEXFILE.Rtex\")" && pdflatex -interaction=scrollmode $RTEXFILE && open -a TeXShop.app $RTEXFILE.pdf

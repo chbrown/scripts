bbc93dabf64be28e6a9fbfd5afe3c4e4a4dbbaf5
Christopher Brown <io@henrian.com>
2013-03-13 10:54:13 -0500

    Niceties for zipf script

--- zipf.py
+++ zipf.py
@@ -6,13 +6,14 @@ from collections import Counter
 parser = argparse.ArgumentParser(description='Token / type comparator.')
 parser.add_argument('--char', action='store_true', help='Count by chars instead of words.')
 parser.add_argument('--case', action='store_true', help='Retain case.')
-parser.add_argument('--top', default=200, help='How many types to print out counts for.')
+parser.add_argument('--top', default=200, type=int, help='How many types to print out counts for. (default: 200)')
 parser.add_argument('--digits', action='store_true', help='Keep digits?')
 args = parser.parse_args()
 
 import re
 word_re = re.compile(r'\w+', re.U)
 
+wc = 0
 types = Counter()
 for raw in sys.stdin:
     line = raw.decode('utf8')
@@ -27,11 +28,11 @@ for raw in sys.stdin:
     if not args.digits:
         tokens = filter(lambda x: not x.isdigit(), tokens)
 
-    # print tokens
+    wc += len(tokens)
     types.update(tokens)
 
+print u'%26s %7s %9s' % ('token', 'count', 'freq')
 for token, count in types.most_common(args.top):
     # if token.iswhitespace():
     # else:
-    print u'%26s (%d)' % (repr(token)[2:-1], count)
-
+    print u'%26s %7d %8.4f%%' % (repr(token)[2:-1], count, count * 100.0 / wc)

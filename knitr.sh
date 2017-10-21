b269864c4b0bef9f752f8212570293a62d9fedb8
Christopher Brown <io@henrian.com>
2017-10-21 12:58:36 -0500

    Remove undocumented knitr.sh script

--- knitr.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-RTEXFILES=($1)
-RTEXFILE=${RTEXFILES[0]/\.Rtex/}
-echo Knitting $RTEXFILE
-Rscript -e "library(knitr); knit(\"$RTEXFILE.Rtex\")" && pdflatex -interaction=scrollmode $RTEXFILE && open -a TeXShop.app $RTEXFILE.pdf
